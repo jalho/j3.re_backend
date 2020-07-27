0112c60aa8d297693ad2d67db917f9dc49f7dda7
jalho <34038726+jalho@users.noreply.github.com>
2020-07-27 21:06:19 +0300

    Add note leaving mutation

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,9 +1,13 @@
+// graphql doesn't come with resolver typings I guess TODO: Consider using https://github.com/ardatan/graphql-tools
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { NoteModel } from "../schema/Mongoose";
 import { Note } from "../types";
 import tg from "../utils/typeGuards"; // type guards
 
 const resolvers = {
   Query: {
+    // TODO: Add a parameter for approval status and query only the approved from MongoDB!
     notes: async (): Promise<Note[]> => {
       const searchResults = await NoteModel.find({});
       const finalResults: Array<Note> = [];
@@ -15,6 +19,22 @@ const resolvers = {
         }
       });
       return finalResults;
+    }
+  },
+  Mutation: {
+    addNote: async (_parent: any, args: { content: string; }): Promise<Note> => {
+
+      const savedNote = await (new NoteModel({
+        approved: Math.random() >= 0.5, // TODO: Set as false by default.
+        content: args.content,
+        time: new Date().toISOString()
+      })).save();
+
+      if (tg.isNote(savedNote)) {
+        return savedNote;
+      } else {
+        throw new Error("The note couldn't be correctly!");
+      }
     }
   }
 };
--- src/schema/GraphQL/NoteSchema.ts
+++ src/schema/GraphQL/NoteSchema.ts
@@ -1,14 +1,24 @@
 import { gql } from "apollo-server";
 
 export const Note = gql`
+  type Note {
+    "Unique ID generated to the note by MongoDB (database)."
+    id: ID!
+    "Note's content."
+    content: String!
+    "Timestamp of leaving the note as determined by the server."
+    time: String!
+    "Whether or not the note has been manually approved, as the internet is a filthy place."
+    approved: Boolean
+  }
+
   type Query {
+    "Get all notes from the database."
     notes: [Note]!
   }
 
-  type Note {
-    id: ID!
-    content: String!
-    time: String!
-    approved: Boolean
+  type Mutation {
+    "Add a new note with given content. ID, timestamp and approval are determined by the system."
+    addNote(content: String): Note
   }
 `;
