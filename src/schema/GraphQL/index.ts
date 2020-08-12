311a8a9eaaa280d95d3b4b59c8d8605e4eb28db0
jalho <34038726+jalho@users.noreply.github.com>
2020-08-12 12:53:07 +0300

    Define Projects in schema

--- src/schema/GraphQL/index.ts
+++ src/schema/GraphQL/index.ts
@@ -28,6 +28,22 @@ const AuthPayload = gql`
   }
 `;
 
+const Project = gql`
+  type Project {
+    id: ID!
+    name: String!
+    "Project's category, e. g. full stack"
+    categories: [String]
+    description: String
+    "Technologies used in project, e. g. GraphQL"
+    technologies: String
+    "E. g. Summer 2020"
+    startTime: String
+    "URLs to e. g. GitHub repositories relevant to the project"
+    repositories: [String]
+  }
+`;
+
 const Queries = gql`
   type Query {
     "Get all users from the database."
@@ -36,6 +52,8 @@ const Queries = gql`
     approvedNotes: [Note]
     "Get one user with matching username."
     oneUser(username: String!): User
+    "Get all projects."
+    projects: [Project]
   }
 `;
 
@@ -47,8 +65,17 @@ const Mutations = gql`
     addNote(content: String!): Note
     "Return authentication payload on correct credentials, otherwise null."
     login(username: String!, password: String!): AuthPayload
+    "Authentication required, otherwise return null."
+    addProject(
+      name: String!,
+      categories: [String],
+      description: String,
+      technologies: [String],
+      startTime: String,
+      repositories: [String]
+    ): Project
   }
 `;
 
 // export all schemas here as one default export array
-export default [Note, User, Queries, Mutations, AuthPayload];
+export default [Note, User, Queries, Mutations, AuthPayload, Project];
--- src/schema/Mongoose/index.ts
+++ src/schema/Mongoose/index.ts
@@ -11,10 +11,29 @@ const userSchema = new mongoose.Schema({
   passwordHash: String
 });
 
+const projectSchema = new mongoose.Schema({
+  name: String,
+  categories: [String],
+  description: String,
+  technologies: [String],
+  startTime: String,
+  repositories: [String]
+});
+
 interface UserDocument extends mongoose.Document {
   username: string;
   passwordHash: string;
 }
 
+interface ProjectDocument extends mongoose.Document {
+  name: string;
+  categories: string[];
+  description: string;
+  technologies: string[];
+  startTime: string;
+  repositories: string[];
+}
+
 export const UserModel = mongoose.model<UserDocument>("User", userSchema);
 export const NoteModel = mongoose.model("Note", noteSchema); // TODO: Use generics in NoteModel too, like in UserModel
+export const ProjectModel = mongoose.model<ProjectDocument>("Project", projectSchema);
